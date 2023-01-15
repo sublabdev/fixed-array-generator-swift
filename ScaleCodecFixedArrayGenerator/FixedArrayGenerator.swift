899ebc1f5a31d3f4aa8b0fc234aa4d0f799500a5
TigranIskandaryan <tiger@sublab.dev>
2023-01-15 04:53:19 +0300

    In the generated String for FixedArrayGenerator added documentation comments

--- ScaleCodecFixedArrayGenerator/FixedArrayGenerator.swift
+++ ScaleCodecFixedArrayGenerator/FixedArrayGenerator.swift
@@ -9,6 +9,7 @@ import Foundation
 
 // MARK: - Array\(size) + Encodable
 
+/// An array with \(size) \(size > 1 ? "elements" : "element")
 @propertyWrapper public struct Array\(size)<Element> {
     public var wrappedValue: [Element]
     public init(wrappedValue: [Element]) {
@@ -19,6 +20,9 @@ import Foundation
 // MARK: - Array\(size) + Encodable
 
 extension Array\(size): Encodable where Element : Encodable {
+    /// Encodes the array via provided encoder
+    /// - Parameters:
+    ///     - encoder: An encoder that is used to encode the array
     public func encode(to encoder: Encoder) throws {
         guard wrappedValue.count == \(size) else { throw FixedArrayError.invalidSize }
         var container = encoder.unkeyedContainer()
@@ -29,6 +33,9 @@ extension Array\(size): Encodable where Element : Encodable {
 // MARK: - Array\(size) + Decodable
 
 extension Array\(size): Decodable where Element : Decodable {
+    /// Creates a fixed array with with \(size) \(size > 1 ? "elements" : "element") using the provided decoder, by decoding its each element
+    /// - Parameters:
+    ///     - decoder: Decoder, used to create the array
     public init(from decoder: Decoder) throws {
         var container = try decoder.unkeyedContainer()
         wrappedValue = try (0..<\(size)).map { _ in try container.decode(Element.self) }
